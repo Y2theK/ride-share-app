cfc4bc3760ce0f8fa540aebf76381726282d7710
Ye Yint Kyaw <yeyintkyaw300@gmail.com>
2023-04-28 20:55:02 +0630

    DrivingView added

--- src/views/DrivingView.vue
+++ src/views/DrivingView.vue
@@ -1,3 +1,74 @@
 <template>
-  <h1>Driving</h1>
+  <div class="pt-16">
+    <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
+    <div>
+      <div
+        class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left"
+      >
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div>
+            <GMapMap
+              :zoom="14"
+              :center="location.current.geometry"
+              ref="gMap"
+              style="width: 100%; height: 256px"
+            >
+              <GMapMarker
+                :position="location.current.geometry"
+                :icon="currentIcon"
+              />
+              <GMapMarker
+                :position="location.destination.geometry"
+                :icon="destinationIcon"
+              />
+            </GMapMap>
+          </div>
+          <div class="mt-2">
+            <p class="text-xl">Going to <strong>pick up a passenger</strong></p>
+          </div>
+        </div>
+        <!-- <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button
+            v-if="trip.is_started"
+            @click="handleCompleteTrip"
+            class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none"
+          >
+            Complete Trip
+          </button>
+          <button
+            v-else
+            @click="handlePassengerPickedUp"
+            class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none"
+          >
+            Passenger Picked Up
+          </button>
+        </div> -->
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+import { useLocationStore } from "@/stores/location";
+import { useTripStore } from "@/stores/trip";
+import { useRouter } from "vue-router";
+import { ref, onMounted, onUnmounted } from "vue";
+const location = useLocationStore();
+const trip = useTripStore();
+const router = useRouter();
+const gMap = ref(null);
+const title = ref("Driving to passenger...");
+const currentIcon = {
+  url: "https://openmoji.org/data/color/svg/1F698.svg",
+  scaledSize: {
+    width: 32,
+    height: 32,
+  },
+};
+const destinationIcon = {
+  url: "https://openmoji.org/data/color/svg/1F920.svg",
+  scaledSize: {
+    width: 24,
+    height: 24,
+  },
+};
+</script>
